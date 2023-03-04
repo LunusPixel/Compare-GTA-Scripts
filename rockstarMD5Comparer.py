bc60c24d4d97760421a892da4175f049c4adff82
SB <87047045+Sup3820@users.noreply.github.com>
2023-03-04 14:32:22 +0000

    Update rockstarMD5Comparer.py
    updated code to be more efficient and easier to read

--- rockstarMD5Comparer.py
+++ rockstarMD5Comparer.py
@@ -1,25 +1,22 @@
 from urllib import request
-from time import sleep
 import hashlib
 import os
-
+import time
 
 CHECK_INTERVAL = 1  # Minutes
 FILE_NAME = "bg_ng_944_1.rpf"
-OLD_MD5_FILE_PATH = "old.txt"
+OLD_MD5_FILE_PATH = os.path.abspath("old.txt")
 FILE_KEY = "F06F12F49B843DADE4A7BE053505B19C9E415C95D93753450A269144D59A0115"
 FILE_URL = "http://prod.cloud.rockstargames.com/titles/gta5/pcros/bgscripts/bg_ng_944_1.rpf"
-
 
 try:
     while True:
         # Download new file and get its MD5
-        new_md5 = hashlib.md5()
-        print("Downloading new", FILE_NAME + "...")
-        request.urlretrieve(FILE_URL, os.path.join(os.getcwd(), FILE_NAME))
-        with open(FILE_NAME, "rb") as file:
-            data = file.read()
-            new_md5.update(data)
+        print(f"Downloading new {FILE_NAME}...")
+        with request.urlopen(FILE_URL) as response, open(FILE_NAME, "wb") as file:
+            data = response.read()
+            file.write(data)
+            new_md5 = hashlib.md5(data).hexdigest()
 
         # Read old MD5 from file
         print("Reading old MD5...")
@@ -28,10 +25,10 @@ try:
 
         # If file is updated, write new MD5 to file
         print("Comparing MD5s...")
-        if old_md5 != new_md5.hexdigest():
-            print(FILE_NAME, "updated!")
+        if old_md5 != new_md5:
+            print(f"{FILE_NAME} updated!")
             with open(OLD_MD5_FILE_PATH, "w") as fp:
-                fp.writelines(new_md5.hexdigest())
+                fp.write(new_md5)
 
             # Insert any other functionality here
 
@@ -39,9 +36,8 @@ try:
             print("No changes found!")
 
         # Sleep until next try
-        print("Sleeping for", CHECK_INTERVAL, "minute(s)...\n")
-        sleep(60 * CHECK_INTERVAL)
+        print(f"Sleeping for {CHECK_INTERVAL} minute(s)...\n")
+        time.sleep(60 * CHECK_INTERVAL)
 
 except Exception as e:
-    print("Error occured", str(e))
-
+    print(f"Error occurred: {str(e)}")
