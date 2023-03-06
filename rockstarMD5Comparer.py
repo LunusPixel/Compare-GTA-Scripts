8481c79b98333aea150e367acf35cb3c3e0e6c57
S <87047045+Sup3820@users.noreply.github.com>
2023-03-06 18:09:28 +0000

    Add files via upload
    In this updated code, I've added the necessary Twitter API keys and access tokens, and authenticated the API using Tweepy. I also added a new section that constructs a tweet text message and calls the 'update_status' method of the API object to send the tweet.

--- rockstarMD5Comparer.py
+++ rockstarMD5Comparer.py
@@ -2,12 +2,24 @@ from urllib import request
 import hashlib
 import os
 import time
+import tweepy
+
+# Twitter API keys and access tokens
+CONSUMER_KEY = "your_consumer_key"
+CONSUMER_SECRET = "your_consumer_secret"
+ACCESS_TOKEN = "your_access_token"
+ACCESS_TOKEN_SECRET = "your_access_token_secret"
 
 CHECK_INTERVAL = 1  # Minutes
 FILE_NAME = "bg_ng_944_1.rpf"
 OLD_MD5_FILE_PATH = os.path.abspath("old.txt")
 FILE_KEY = "F06F12F49B843DADE4A7BE053505B19C9E415C95D93753450A269144D59A0115"
 FILE_URL = "http://prod.cloud.rockstargames.com/titles/gta5/pcros/bgscripts/bg_ng_944_1.rpf"
+
+# Authenticate Twitter API
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
 
 try:
     while True:
@@ -23,14 +35,16 @@ try:
         with open(OLD_MD5_FILE_PATH, "r") as fp:
             old_md5 = fp.readline()
 
-        # If file is updated, write new MD5 to file
+        # If file is updated, write new MD5 to file and tweet about it
         print("Comparing MD5s...")
         if old_md5 != new_md5:
             print(f"{FILE_NAME} updated!")
             with open(OLD_MD5_FILE_PATH, "w") as fp:
                 fp.write(new_md5)
 
-            # Insert any other functionality here
+            # Tweet about the update
+            tweet_text = f"🚨 {FILE_NAME} has been updated! 🚨"
+            api.update_status(tweet_text)
 
         else:
             print("No changes found!")
