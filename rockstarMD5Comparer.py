dc152cdbf17d915faecf5053030cb81a3bc7bea3
S <87047045+Sup3820@users.noreply.github.com>
2023-03-06 19:35:54 +0000

    Add files via upload
    Moved constants to the top and given them descriptive names
    Set credentials for Twitter API and Discord webhook in separate dictionaries and a separate variable, respectively
    Moved authentication for Twitter API to its own block
    Defined a function to send notifications to Discord webhook
    Used requests module instead of urllib to send HTTP requests to the Discord webhook URL
    Simplified the condition in the main loop to check if the file is updated

--- rockstarMD5Comparer.py
+++ rockstarMD5Comparer.py
@@ -3,23 +3,35 @@ import hashlib
 import os
 import time
 import tweepy
+import requests
 
-# Twitter API keys and access tokens
-CONSUMER_KEY = "your_consumer_key"
-CONSUMER_SECRET = "your_consumer_secret"
-ACCESS_TOKEN = "your_access_token"
-ACCESS_TOKEN_SECRET = "your_access_token_secret"
-
+# Set constants
 CHECK_INTERVAL = 1  # Minutes
 FILE_NAME = "bg_ng_944_1.rpf"
 OLD_MD5_FILE_PATH = os.path.abspath("old.txt")
 FILE_KEY = "F06F12F49B843DADE4A7BE053505B19C9E415C95D93753450A269144D59A0115"
 FILE_URL = "http://prod.cloud.rockstargames.com/titles/gta5/pcros/bgscripts/bg_ng_944_1.rpf"
 
-# Authenticate Twitter API
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-api = tweepy.API(auth)
+# Set credentials for Twitter API and Discord webhook
+twitter_credentials = {
+    "consumer_key": "your_consumer_key",
+    "consumer_secret": "your_consumer_secret",
+    "access_token": "your_access_token",
+    "access_token_secret": "your_access_token_secret"
+}
+discord_webhook_url = "your_discord_webhook_url"
+
+# Authenticate Twitter API and create API object
+auth = tweepy.OAuthHandler(twitter_credentials["consumer_key"], twitter_credentials["consumer_secret"])
+auth.set_access_token(twitter_credentials["access_token"], twitter_credentials["access_token_secret"])
+twitter_api = tweepy.API(auth)
+
+# Define function to send notification to Discord webhook
+def send_discord_notification(webhook_url, message):
+    data = {"content": message}
+    response = requests.post(webhook_url, json=data)
+    if response.status_code != 204:
+        print(f"Failed to send Discord notification. Status code: {response.status_code}")
 
 try:
     while True:
@@ -35,16 +47,20 @@ try:
         with open(OLD_MD5_FILE_PATH, "r") as fp:
             old_md5 = fp.readline()
 
-        # If file is updated, write new MD5 to file and tweet about it
+        # If file is updated, write new MD5 to file and send notifications
         print("Comparing MD5s...")
         if old_md5 != new_md5:
             print(f"{FILE_NAME} updated!")
             with open(OLD_MD5_FILE_PATH, "w") as fp:
                 fp.write(new_md5)
 
-            # Tweet about the update
+            # Send tweet about the update
             tweet_text = f"🚨 {FILE_NAME} has been updated! 🚨"
-            api.update_status(tweet_text)
+            twitter_api.update_status(tweet_text)
+
+            # Send Discord notification about the update
+            discord_notification_text = f"**{FILE_NAME} has been updated!**"
+            send_discord_notification(discord_webhook_url, discord_notification_text)
 
         else:
             print("No changes found!")
